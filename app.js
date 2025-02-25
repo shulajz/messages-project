6ab5cc053fc16f3a98cb826e28226e2f7022538d
shulajz <shulajz@gmail.com>
2025-02-25 22:14:42 +0200

    remove comment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import messageRoutes from "./routes/messages.js";
 
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON
+app.use(express.json());
 app.use("/messages", messageRoutes);
 
 export default app;
